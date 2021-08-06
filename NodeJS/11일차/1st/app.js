d964dc09e04d8ceac1aa1b088a16f9d3a0faf687
cscculture <cscculture95@gmail.com>
2021-08-06 21:13:33 +0900

    NodeJS 스터디 12일차 DB + 사이트 완성

--- NodeJS/11일차/1st/app.js
+++ NodeJS/11일차/1st/app.js
@@ -9,10 +9,15 @@ const nunjucks = require('nunjucks');
 
 const { sequelize } = require('./models');
 
+// 라우터를 가져온다.
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
-nunjucks.configure('view', {
+nunjucks.configure('views', {
     express : app,
     watch : true,
 });
@@ -31,6 +36,10 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
--- NodeJS/11일차/1st/public/sequelize.js
+++ NodeJS/11일차/1st/public/sequelize.js
@@ -80,13 +80,94 @@ async function getComment(id) {
                 }
 
                 try {
-                    
+                    // REST API
+                    // post : 데이터를 보낸다.
+                    // patch : 일부를 수정한다.
+                    await axios.patch(`/comments/${comment.id}`, { comment : newComment });
+                    getComment(id);
                 } catch(err) {
-
+                    console.error(err);
                 }
-            })
+            });
+
+            // 삭제 기능
+            const del = document.createElement('button');
+            del.textContent = '삭제';
+            del.addEventListener('click', async () => {
+                try {
+                    await axios.delete(`/comments/${comment.id}`);
+                    getComment(id);
+                } catch(err) {
+                    console.error(err);
+                }
+            });
+
+            td = document.createElement('td');
+            td.appendChild(edit);
+            row.appendChild(td);
+
+            
+            td = document.createElement('td');
+            td.appendChild(del);
+            row.appendChild(td);
+
+            tbody.appendChild(row);
         });
     } catch(err) {
         console.error(err);
     }
 }
+
+// 사용자 등록
+document.getElementById('user-form').addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const name = e.target.username.value;
+    const age = e.target.age.value;
+    const married = e.target.married.checked;
+
+    if(!name) {
+        return alert('이름을 입력하세요');
+    }
+
+    if(!age) {
+        return alert('나이를 입력하세요');
+    }
+
+    try {
+        await axios.post('/users', { name, age, married });
+        getUser();
+    } catch(err) {
+        console.error(err);
+    }
+
+    e.target.username.value = '';
+    e.target.age.value = '';
+    e.target.married.checked = false;
+});
+
+// 댓글 등록
+document.getElementById('comment-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const id = e.target.userid.value;
+    const comment = e.target.comment.value;
+
+    if (!id) {
+        return alert('아이디를 입력하세요');
+    }
+
+    if(!comment) {
+        return alert('댓글을 입력하세요');
+    }
+
+    try {
+        await axios.post('/comments', { id, comment });
+        getComment(id);
+    } catch(err) {
+        console.error(err);
+    }
+
+    e.target.userid.value = '';
+    e.target.comment.value = '';
+});
